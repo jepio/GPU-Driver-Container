29eacaf17ad60905eb37d3e3848f5bf98b352d59
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2024-03-11 12:20:53 +0100

    wip

--- flatcar/update.sh
+++ flatcar/update.sh
@@ -2,11 +2,11 @@
 
 set -x
 
+FLATCAR=3815.2.0
 docker rm -f nvidia-driver
 docker run -it --privileged --pid=host \
     -v /run/nvidia:/run/nvidia:shared \
     -v /var/log:/var/log \
     -v /dev/log:/dev/log \
-    -v /etc/os-release:/host-etc/os-release \
     --name nvidia-driver \
-    nvidia/nvidia-driver-flatcar:latest update
+    nvidia/nvidia-driver-flatcar:latest update -f ${FLATCAR}
